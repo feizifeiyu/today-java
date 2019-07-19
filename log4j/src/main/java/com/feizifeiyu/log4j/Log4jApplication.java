59fd51d9a4b2b2ea4d66ee115242e6d13ecc59d3
vincent <961919392@qq.com>
2019-07-19 16:19:30 +0800

    多模块项目-初始化状态

---
+++ log4j/src/main/java/com/feizifeiyu/log4j/Log4jApplication.java
@@ -0,0 +1,17 @@
+package com.feizifeiyu.log4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 非子非鱼
+ * @date: 2019/7/19 下午3:08
+ */
+@SpringBootApplication
+public class Log4jApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Log4jApplication.class, args);
+	}
+
+}
---
+++ logback/src/main/java/com/feizifeiyu/logback/LogbackApplication.java
@@ -0,0 +1,17 @@
+package com.feizifeiyu.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: 非子非鱼
+ * @date: 2019/7/19 下午3:08
+ */
+@SpringBootApplication
+public class LogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LogbackApplication.class, args);
+    }
+
+}
